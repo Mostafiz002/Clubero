73b451e053457b71b9f70ebb65f9a9a51b80d2a6
Mostafiz002 <mo.mahin4@gmail.com>
2025-12-09 00:40:40 +0600

    feat: google login added

--- src/components/shared/GoogleBtn.jsx
+++ src/components/shared/GoogleBtn.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { useLocation, useNavigate } from "react-router";
+import useAuth from "../../hooks/useAuth";
+import toast from "react-hot-toast";
 
 const GoogleBtn = () => {
+  const { googleSignIn } = useAuth();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleGoogleSignin = () => {
+    try {
+      googleSignIn().then(() => {
+        toast.success("Logged in successfully");
+        navigate(location?.state || "/");
+        window.scrollTo(0, 0);
+      });
+    } catch {
+      toast.error("Google sign-in failed. Please try again.");
+    }
+  };
+
   return (
     <div>
-      <button className="py-3 rounded-full w-full  cursor-pointer bg-base-300/50 hover:bg-base-300/70 flex items-center gap-2 justify-center">
+      <button
+        onClick={handleGoogleSignin}
+        className="py-3 rounded-full w-full  cursor-pointer bg-base-300/50 hover:bg-base-300/70 flex items-center gap-2 justify-center"
+      >
         <FcGoogle size={22} />
         Login with Google
       </button>
